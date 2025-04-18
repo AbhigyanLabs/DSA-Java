94db2b73315b75c8abeadd06430784ed1577e9b0
Abhiyan Singh <abhigyansingh47@gmail.com>
2025-04-18 17:24:11 +0530

    Removed all files to clean the repository

--- 53 (Strings)/substrings.java
+++
@@ -1,9 +0,0 @@
-class substrings {
-    public static void main(String[] args) {
-        String s = "physics";
-        
-        for (int j = 2; j < 4; j++) {
-            System.out.println(s.substring(j) + " ");
-        }
-    }
-}
--- 55-57 (Linked List)/tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-System.out.println("Reversed Linked list is:");
-        Node q=Reverse(a);
-        Display(q);
-        System.out.println();
